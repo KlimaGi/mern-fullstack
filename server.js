4943a2215d2c810f08964eaba0e519e598aff99d
Giedre <giedre.klima@gmail.com>
2021-11-21 19:26:54 +0200

    creating an express server

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
-import config, { nodeEnv, logStars } from "./config";
+import config from "./config";
+import apiRouter from "./api/index";
 
-console.log(config, nodeEnv);
-logStars("Function");
+import express from "express";
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+server.use("/api", apiRouter);
+server.use(express.static("public"));
+
+server.listen(config.port, () => {
+  console.info("Express listening on port ", config.port);
+});
