0b2cb7dd37f1d9fe708e44429a7c0f629b087891
Giedre <giedre.klima@gmail.com>
2021-11-24 20:41:32 +0200

    fix the checksum problem - solution-2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
 import App from "./components/App";
 
-axios
-  .get("/api/contests")
-  .then((resp) => {
-    ReactDOM.render(
-      <App initialContests={resp.data.contests} />,
-      document.getElementById("root")
-    );
-    // this.setState({
-    //   contests: resp.data.contests,
-    // });
-  })
-  .catch(console.error);
+ReactDOM.render(
+  <App initialContests={window.initialData.contests} />,
+  document.getElementById("root")
+);
