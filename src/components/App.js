7172b4d149b5c2d3fb00ff79d474c82a897684a0
Giedre <giedre.klima@gmail.com>
2021-11-26 15:59:04 +0200

    navigating to a list of contest

--- src/components/App.js
+++ src/components/App.js
@@ -30,6 +30,17 @@ class App extends React.Component {
       });
     });
   };
+
+  fetchContestList = () => {
+    pushState({ currentContestId: null }, "/");
+    api.fetchContestList().then((contests) => {
+      this.setState({
+        currentContestId: null,
+        contests,
+      });
+    });
+  };
+
   currentContest() {
     return this.state.contests[this.state.currentContestId];
   }
@@ -42,7 +53,12 @@ class App extends React.Component {
   }
   currentContent() {
     if (this.state.currentContestId) {
-      return <Contest {...this.currentContest()} />;
+      return (
+        <Contest
+          contestListClick={this.fetchContestList}
+          {...this.currentContest()}
+        />
+      );
     }
     return (
       <ContestList
